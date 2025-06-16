a3c305c042d6eb85dde0a42de6936a353791da9a
Phouychhay Lim <lim.phouychhay@cellcard.com.kh>
2025-06-16 16:27:20 +0700

    feat: add vercel analytic & speed insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 import "./globals.css";
 
@@ -28,6 +30,8 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
+        <SpeedInsights />
         {children}
       </body>
     </html>
